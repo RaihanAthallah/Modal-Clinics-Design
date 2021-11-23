befea87beeb97628a9c571e9cbfc5acadfbf4035
RaihanAthallah <rraihan1947@gmail.com>
2021-11-23 20:57:18 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function initMap() {
+  var coordinates = { lat: -6.1872, lng: 106.8322 }; //koordinat letak
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 20,
+    center: coordinates,
+  });
+  var marker = new google.maps.Marker({
+    position: coordinates,
+    map: map,
+  });
+}
+
+function copyFunction() {
+  const copyText = document.getElementById("contact").textContent;
+  console.log(copyText);
+  navigator.clipboard.writeText(copyText);
+  alert("Nomor Telepon Telah Dicopy: " + copyText);
+}
